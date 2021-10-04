c7d29a79dd7661e76a062a7fdf4990983a8ef24a
djelalswns12 <djelalswns12@naver.com>
2021-10-04 18:06:25 +0900

    캐릭터 이동 구현
    벽타기
    이동
    점프

--- Assets/Script/Unit/UnitScript.cs
+++ Assets/Script/Unit/UnitScript.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 public class UnitScript : MonoBehaviour
 {
     protected Rigidbody2D rigid;
-    public float speed;
+    private float _speed;
+    public float Speed
+    {
+        get
+        {
+            return _speed;
+        }
+        set
+        {
+            _speed = value;
+        }
+    }
+    private float jumpPower;
+    public float jumpingJumpPower;
+    public float walkingJumpPower;
+    public float setSpeed;
+    public float walkingSpeed;
+    public float jumpingSpeed;
+    public int maxJump;
+    bool isOnFloor;
+    public Collider2D footColider;
+    public LayerMask floorLayer;
+    public Vector3 groundChkPos;
+    public Vector3 groundChkSize;
+    public Vector3 leftWallChkPos;
+    public Vector3 rightWallChkPos;
+    public Vector3 wallChkSize;
     // Start is called before the first frame update
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void Update()
-    {
-
-    }
-    private void FixedUpdate()
-    {
-        
-    }
     public void SetMoveKeys()
     {
         SetKey("A", MoveLeft,MoveLeftCancel);
         SetKey("D", MoveRight, MoveRightCancel);
+        SetKey("W", MoveUp, MoveUpCancel);
     }
     public void SetKey(string key,Command.Msg msg,Command.Msg unMsg)
     {
@@ -36,11 +55,15 @@ public class UnitScript : MonoBehaviour
     }
     protected virtual void MoveUpCancel()
     {
-        Debug.Log(gameObject.name + " Up");
+        Debug.Log(gameObject.name + " Up InActive");
     }
     protected virtual void MoveDown()
     {
         Debug.Log(gameObject.name + " Down");
+    }
+    protected virtual void MoveDownCancel()
+    {
+        Debug.Log(gameObject.name + " Down InActive");
     }
     protected virtual void MoveLeft()
     {
@@ -57,5 +80,101 @@ public class UnitScript : MonoBehaviour
     protected virtual void MoveRightCancel()
     {
         Debug.Log(gameObject.name + " Right InActive");
+    }
+    protected virtual void OnFloorEvent()
+    {
+
+    }
+    public bool UnitsOnLeftWall()
+    {
+        var wall = Physics2D.OverlapBox(transform.position + leftWallChkPos, wallChkSize, 0f, floorLayer);
+        if (wall != null)
+        {
+            var sp = wall.gameObject.GetComponent<SpriteRenderer>();
+            Debug.DrawRay(new Vector3(wall.transform.position.x+(sp.bounds.size.x/2), wall.transform.position.y, 0), Vector3.left,Color.green);
+            if(transform.position.x < wall.transform.position.x + (sp.bounds.size.x / 2) && transform.position.x > wall.transform.position.x - (sp.bounds.size.x / 2) )
+            {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    public bool UnitsOnRightWall()
+    {
+        var wall = Physics2D.OverlapBox(transform.position + rightWallChkPos, wallChkSize, 0f, floorLayer);
+        if (wall != null)
+        {
+            var sp = wall.gameObject.GetComponent<SpriteRenderer>();
+            Debug.DrawRay(new Vector3(wall.transform.position.x - (sp.bounds.size.x / 2), wall.transform.position.y, 0), Vector3.right, Color.green);
+            if (transform.position.x < wall.transform.position.x + (sp.bounds.size.x / 2) && transform.position.x > wall.transform.position.x - (sp.bounds.size.x / 2) )
+            {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    public bool UnitIsOnFloor()
+    {
+        if (Mathf.Floor(rigid.velocity.y) > 0)
+        {
+            return false;
+        }
+        var floors = Physics2D.OverlapBoxAll(transform.position+groundChkPos, groundChkSize, 0f,floorLayer);  
+        if (floors.Length>0)
+        {
+            Collider2D floor=floors[0];
+            foreach (var c in floors)
+            {
+                //세워져있는 땅과 아래땅을 구분하기 위해 가장 밑에있는 땅을 찾는다
+                if (c.transform.position.y<floor.transform.position.y)
+                {
+                    floor = c;
+                }
+            }
+            var sp = floor.gameObject.GetComponent<SpriteRenderer>();
+            Debug.DrawRay(new Vector3(floor.transform.position.x,floor.transform.position.y + (sp.bounds.size.y / 2*0.88f),0), Vector3.down*0.2f,Color.blue);
+            if ((floor.transform.position.y + (sp.bounds.size.y / 2*0.88f)) < transform.position.y+groundChkPos.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void SetJumpPower(float power)
+    {
+        jumpPower = power;
+    }
+    public float GetJumpPower()
+    {
+        return jumpPower;
+    }
+    public float GetSpeed(string type)
+    {
+        float nowSpeed = setSpeed;
+        if (type == "Walk")
+        {
+            nowSpeed *= walkingSpeed;
+        }
+        else
+        {
+            nowSpeed *= jumpingSpeed;
+        }
+        return nowSpeed;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position + groundChkPos, groundChkSize);
+        Gizmos.DrawCube(transform.position + leftWallChkPos, wallChkSize);
+        Gizmos.DrawCube(transform.position + rightWallChkPos, wallChkSize);
     }
 }
